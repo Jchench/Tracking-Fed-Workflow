2e845ede9717f292bceb41a5bcf69ca99b1570c7
Jchench <jessiechen2026@u.northwestern.edu>
2024-07-09 13:52:01 -0400

    visualization

--- 01_compile.R
+++ 01_compile.R
@@ -1,7 +1,7 @@
 library(dplyr)
 
 # Set the path to your folder
-folder_path <- "1986-2000/csv/combine"
+folder_path <- "cleaned_csv"
 
 # List all CSV files in the folder
 file_list <- list.files(path = folder_path, pattern = "*.csv", full.names = TRUE)
@@ -12,14 +12,5 @@ data_frames <- lapply(file_list, read.csv)
 # Combine all data frames into one
 combined_changes <- do.call(rbind, data_frames)
 
-# Group and summarize the combined data frame
-combined_changes <- combined_changes |> 
-  group_by(Part, Change, FedReg_Number, Year) |> 
-  summarize(Page = paste(Page, collapse = ", ")) |> 
-  ungroup()
-
-combined_changes <- combined_changes |> 
-  select(Part, Change, Page, FedReg_Number, Year)
-
 # Write the combined data frame to a CSV file
-write.csv(combined_changes, file = "cleaned_csv/1986_2000.csv", row.names = FALSE)
+write.csv(combined_changes, file = "cleaned_csv/1964-2023.csv", row.names = FALSE)
--- 05_visualization_2.R
+++ 05_visualization_2.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(ggthemes)
 
 # Read the data
-d <- read_csv("cleaned_csv/2001-.csv")
+d <- read_csv("cleaned_csv/1964_1972.csv")
 regs <- read_csv("Federal_Reserve_Board_Regulations.csv") %>%
   filter(`Part Number` < 250)
 
@@ -32,13 +32,40 @@ all_parts <- sort(unique(c(activity$Part, regs$`Part Number`)))
 activity$Part <- factor(activity$Part, levels = all_parts)
 
 # Plot the data
-ggplot(activity, aes(x = Year + approx_page_frac, y = Part, shape = Action)) +
-  geom_point(alpha = 0.5, size = 3) +
+option_1 <- 
+  ggplot(activity, aes(x = Year + approx_page_frac, y = Part, color = Action, shape = Action)) +
+  geom_point(size = 3) +
   theme_tufte() +
   ggtitle("Activity by Part and Action Type") +
   xlab("Year") +
   scale_y_discrete(limits = all_parts) +
+  scale_color_manual(values = c("Addition" = "green", "Deletion" = "red", "Modification" = "blue", "Other" = "grey")) +
   scale_shape_manual(values = c("Addition" = 16, "Deletion" = 17, "Modification" = 18, "Other" = 15))
+
+option_1
+
+option_2 <- 
+  ggplot(activity, aes(x = Year + approx_page_frac, y = Part, shape = Action, size = Action)) +
+  geom_point(alpha = 0.5) +
+  theme_tufte() +
+  ggtitle("Activity by Part and Action Type") +
+  xlab("Year") +
+  scale_y_discrete(limits = all_parts) +
+  scale_shape_manual(values = c("Addition" = 16, "Deletion" = 17, "Modification" = 18, "Other" = 15)) +
+  scale_size_manual(values = c("Addition" = 3, "Deletion" = 4, "Modification" = 5, "Other" = 2))
+
+option_2
+
+option_3 <- 
+  ggplot(activity, aes(x = Year + approx_page_frac, y = Part, shape = Action, size = Action)) +
+  geom_point(size = 2) +
+  theme_tufte() +
+  ggtitle("Activity by Part and Action Type") +
+  xlab("Year") +
+  scale_y_discrete(limits = all_parts) +
+  scale_shape_manual(values = c("Addition" = 3, "Deletion" = 4, "Modification" = 19, "Other" = 18))
+
+option_3
 
 # Get unique changes where Subpart is NA
 unique_changes <- d2 %>%
